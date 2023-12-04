fe5c58368aaaa64ec32d274372a51dd3bc14ef4a
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-12-04 21:35:02 +0500

    Created RMAPICacheManager

---
+++ RickAndMortyApp/Supporting Files/RMAPICacheManager.swift
@@ -0,0 +1,14 @@
+//
+//  RMAPICacheManager.swift
+//  RickAndMortyApp
+//
+//  Created by Ислам Пулатов on 12/4/23.
+//
+
+import Foundation
+
+/// Manages in memory session scoped API  cashes
+final class RMAPICacheManager {
+    
+    init() {}
+}
