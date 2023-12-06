eca70530120092bec3a854b4ba841ce0da10cb87
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-12-06 21:47:00 +0500

    Start to create Episode VC

--- RickAndMortyApp/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMortyApp/Controllers/Core/RMEpisodeViewController.swift
@@ -10,10 +10,58 @@ import UIKit
 ///  Controller to show   Episodes
 final class RMEpisodeViewController: UIViewController {
 
+    private let episodeListView = RMEpisodeListView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Episodes"
-        view.backgroundColor = .systemBackground
+        setViews()
+    }
+    
+    private func setViews() {
+        setController()
+        setDelegates()
+        addSubViews()
+        setConstrains()
     }
 
 }
+
+extension RMEpisodeViewController {
+    
+    private func setController() {
+        title = "Episode"
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setDelegates() {
+        episodeListView.delegate = self
+    }
+    
+    private func addSubViews() {
+        view.addSubview(episodeListView)
+    }
+    
+    private func setConstrains() {
+        NSLayoutConstraint.activate([
+        
+            episodeListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            episodeListView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            episodeListView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            episodeListView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+}
+
+//  MARK: - Extension RMEpisodeListViewDelegate
+
+extension RMEpisodeViewController: RMEpisodeListViewDelegate {
+    func rmEpisodeListView(_ characterListView: RMEpisodeListView, didSelectEpisode character: RMEpisode) {
+        let detailVC = RMEpisodeDetailViewController(url: URL(string: character.url))
+        detailVC.navigationItem.largeTitleDisplayMode = .never
+        
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
+
